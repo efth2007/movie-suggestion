9e789dc1139e0dd67bf164acc6fd04b7fa7993a6
efth2007 <efth2007@hotmail.com>
2022-05-18 12:16:13 +0200

    Background pic

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const app = Vue.createApp({
    
     data(){
         return {
+            title: 'Mr',
             firstName: 'Bob',
             lastName: 'Johnson',
-            gender: 'female',
+            gender: 'male',
+            phone: '016974 88307',
             email: 'bob.johnson@gmail.com',
-            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+            backgroundPic: 'bla'
         }
 },
 methods: {
@@ -15,15 +18,31 @@ methods: {
        const { results } = await res.json()
 
        console.log(results)
-       
+       this.title = results[0].name.title,
        this.firstName = results[0].name.first,
         this.lastName = results[0].name.last,
         this.gender = results[0].gender,
+        this.phone = results[0].phone,
         this.email = results[0].email,
         this.picture = results[0].picture.large
 
-    }
+    },
+    async getPicture(){
+        const res = await fetch('https://picsum.photos/id/0/info')
+        const results = await res.json()
+
+        this.backgroundPic=results.download_url
+ 
+       console.log(`Image fetched: ${this.backgroundPic}`)
+
+ 
+        console.log(`IMage fetched:`)
+
+       // this.title = results[0].name.title
+     }
 }
 })
 
 app.mount('#app')
+
+//background-image: url("https://picsum.photos/id/457/2000/1500"
